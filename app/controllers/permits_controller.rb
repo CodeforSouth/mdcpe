58f6eb28c69f8ca3d4457b3f25890f45eaba241d
Ricky Gonzalez Jr <rickygonzjr@gmail.com>
2015-08-04 18:56:13 -0400

    removed bootstrap, fixed some issues

--- app/controllers/permits_controller.rb
+++ app/controllers/permits_controller.rb
@@ -5,4 +5,7 @@ class PermitsController < ApplicationController
 
   def help
   end
+
+  def results
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root                'permits#home'
   get 'home'      =>  'permits#home'
   get 'help'      =>  'permits#help'
+  get 'results'   =>  'permits#results'
 
 end
