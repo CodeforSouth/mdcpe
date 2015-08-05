ce1b14a425c5703eeac58d02c7c130e8e756bd14
Ricky Gonzalez Jr <rickygonzjr@gmail.com>
2015-08-04 20:50:57 -0400

    got permit codes to show

--- app/controllers/permits_controller.rb
+++ app/controllers/permits_controller.rb
@@ -6,7 +6,12 @@ class PermitsController < ApplicationController
   end
 
   def show_business
+    @items
     @categories = Category.all
+    @codes = Permit.where(proposedusedescription: @items).select(:categories).distinct.pluck(:categories)
+  end
+  def self.permits_for category
+    Permit.where(proposedusedescription: category).select(:categories).distinct.pluck(:categories)
   end
 
   def show_permits
